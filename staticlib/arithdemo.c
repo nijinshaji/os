87fc2cbe172978d5b678b54bdaed6553978a0c6a
NIJIN SHAJI <mailmenijin007@gmail.com>
2020-10-19 16:31:20 +0530

    created a static library with extension *.a

---
+++ staticlib/arithdemo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int sub(int ,int );
+int add(int , int );
+int main(int argc , char argv[])
+{
+	printf("addition : %d\n",add(10,20));
+	printf("subtraction: %d\n",sub(10,20));
+			return 0;
+
+			}
