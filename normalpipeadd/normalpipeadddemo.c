e36a9785e0686d0a4da132ca42dd5eecee7b9d1b
NIJIN SHAJI <mailmenijin007@gmail.com>
2020-11-04 19:53:51 +0530

    assignment done to find the sum of 2 integers send from one process and recieved in another using parent and child process

---
+++ normalpipeadd/normalpipeadddemo.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int x[2];
+	pid_t id;
+	int pfd[2];
+	pipe(pfd);
+	id=fork();
+	if(0==id){
+		close(pfd[1]);
+		read(pfd[0],x, sizeof(x));
+		for(int i=0;i<2;i++)
+        	printf("Child(%d) received value: %d\n", getpid(),x[i]);
+		printf("child : the sum is %d\n",(x[0]+x[1]));
+		close(pfd[0]);
+
+	}
+	else{
+		close(pfd[0]);
+		x[0]=1;
+		x[1]=2;
+        	write(pfd[1], x, sizeof(x));
+		for(int i=0;i<2;i++)
+		{
+        	printf("Parent(%d) send value: %d\n", getpid(),x[i]);
+		}
+		close(pfd[1]);
+
+	}
+	return 0;
+	}
